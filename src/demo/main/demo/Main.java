24b8600c750b5ac96aa4a5fb2dd453ea9ac0f7cd
Christian Stein <sormuras@gmail.com>
2025-04-25 17:08:19 +0200

    Prepare starfield

--- src/demo/main/demo/Main.java
+++ src/demo/main/demo/Main.java
@@ -1,51 +1,58 @@
 package demo;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.lang.module.ModuleDescriptor;
 import java.lang.module.ModuleFinder;
 import java.lang.module.ModuleReference;
 import java.nio.file.Path;
-import java.util.StringJoiner;
+import java.util.function.Consumer;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 public class Main {
   public static void main(String[] args) {
     SwingUtilities.invokeLater(Main::createAndShowGUI);
+    spoolSystemInformation(System.out::println);
   }
 
   private static void createAndShowGUI() {
+    var panel = new JPanel();
+    panel.setOpaque(true);
+    panel.setBackground(Color.MAGENTA);
+    panel.setPreferredSize(new Dimension(640, 480));
+
     var frame = new JFrame("bach-demo-cheerpj");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(640, 480);
-    frame.getContentPane().add(new JScrollPane(new JTextArea(computeText())));
+    frame.setUndecorated(true);
+    frame.getContentPane().add(panel);
+    frame.pack();
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+    frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
   }
 
-  private static String computeText() {
-    var joiner = new StringJoiner("\n");
+  private static void spoolSystemInformation(Consumer<String> printer) {
     try {
       var location = Main.class.getProtectionDomain().getCodeSource().getLocation();
-      joiner.add(location.toString());
+      printer.accept(location.toString());
       ModuleFinder.of(Path.of(location.toURI())).findAll().stream()
           .map(ModuleReference::descriptor)
           .map(ModuleDescriptor::toNameAndVersion)
-          .forEach(joiner::add);
+          .forEach(printer);
     } catch (Exception exception) {
-      joiner.add(exception.getMessage());
+      printer.accept(exception.getMessage());
     }
-    joiner.add("");
+    printer.accept("");
     ModuleLayer.boot().modules().stream()
         .map(Module::getDescriptor)
         .sorted()
         .map(ModuleDescriptor::toNameAndVersion)
-        .forEach(joiner::add);
-    joiner.add("");
+        .forEach(printer);
+    printer.accept("");
     System.getProperties().stringPropertyNames().stream()
         .sorted()
-        .forEach(key -> joiner.add(key + " -> " + System.getProperty(key)));
-    return joiner.toString();
+        .forEach(key -> printer.accept(key + " -> " + System.getProperty(key)));
   }
 }
