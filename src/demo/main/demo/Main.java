a1711ed90c038ec64cba204fcccc91a6ff3156f9
Christian Stein <sormuras@gmail.com>
2025-04-25 10:57:43 +0200

    Show system properties

--- src/demo/main/demo/Main.java
+++ src/demo/main/demo/Main.java
@@ -1,6 +1,7 @@
 package demo;
 
 import javax.swing.*;
+import java.util.StringJoiner;
 
 public class Main {
   public static void main(String[] args) {
@@ -11,8 +12,19 @@ public class Main {
     var frame = new JFrame("bach-demo-cheerpj");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.setSize(400, 300);
-    frame.getContentPane().add(new JLabel(frame.getSize().toString(), SwingConstants.CENTER));
+    frame.getContentPane().add(new JScrollPane(new JTextArea(computeText())), SwingConstants.CENTER);
     frame.setLocationRelativeTo(null);
     frame.setVisible(true);
+  }
+
+  private static String computeText() {
+    var joiner = new StringJoiner("\n");
+    try {
+      joiner.add(Main.class.getProtectionDomain().getCodeSource().getLocation().toString());
+    } catch (Exception exception) {
+      joiner.add(exception.getMessage());
+    }
+    System.getProperties().stringPropertyNames().stream().sorted().forEach(key -> joiner.add(key + " -> " + System.getProperty(key)));
+    return joiner.toString();
   }
 }
